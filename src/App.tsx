ac8f1b0f58c1ddfb541d79c72c7c23424aa3f2f3
sanjar003 <sanjarsamatov0306@gmail.com>
2024-03-16 23:36:02 +0600

    delete-text

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,7 @@ function App() {
     <>
       <Provider store={store}>
         <MainRoutes />
-        <div>
-          <h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatum quos veniam maiores, voluptatibus quae neque, unde
-              beatae illum omnis ducimus earum at atque, fugiat facere
-              architecto esse perspiciatis distinctio dignissimos!
-            </p>
-          </h1>
-        </div>
+ 
       </Provider>
     </>
   );
