04abeb41c6d8094221c2ba1c4077745fa98bb69f
sanjar003 <sanjarsamatov0306@gmail.com>
2024-03-18 17:55:55 +0600

    add madol

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGetProductsQuery } from "../../redux/api/productApi";
+import Modal from "../../components/forms/modal/Modal";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const { data: products = [] } = useGetProductsQuery();
+
+  const handleModal = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const isAuth = localStorage.getItem("isAuth");
@@ -20,11 +26,16 @@ const Home: React.FC<HomeProps> = () => {
     localStorage.removeItem("isAuth");
     navigate("/login");
   };
-  return <div>
-    {products.map((el:any) => {
-      return <div>{el.productName}</div>
-    })}
-    <button onClick={logout}>Выйти</button>
-  </div>;
+  return (
+    <div>
+      {products.map((el: any) => {
+        return <div>{el.productName}</div>;
+      })}
+      <button onClick={logout}>Выйти</button>
+      <Modal isOpen={isOpen} onClose={handleModal}>
+        hello my friend
+      </Modal>
+    </div>
+  );
 };
 export default Home;
